43f93dfa6ceed8c667eec2f4d93623374e6e3907
primedildaar2 <primedildaar2@gmail.com>
2024-02-20 11:58:00 +0200

    added the CheckoutCart componoent and displayed it

---
+++ components/CheckoutCart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheckoutCart(){
+  return (
+    <div className={"fixed right-3 bottom-3 bg-white rounded-full"} style={{padding:"0.5rem 1.5rem"}}><span className={"rounded-full"} style={{border:"1px solid rgba(134,239,172,1)",padding:"0.3rem 0.3rem"}}>🛒</span></div>
+  )
+}
+
+export default CheckoutCart
