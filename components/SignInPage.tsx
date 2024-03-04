18a745aeefe3eb36e248f36d83a73e63d8fe4881
primedildaar2 <primedildaar2@gmail.com>
2024-03-04 10:08:19 +0200

    inserted the onchange function for the input boxes and creating the function now

--- components/SignInPage.tsx
+++ components/SignInPage.tsx
@@ -2,30 +2,37 @@
 import React, { useState } from 'react'
 import { User } from '@/app/api/users/data'
 const SignInPage = function(){
-    const [formData,setFormData] = useState<User>({id:Date.now(),
-        username:"",
-        email:"",
-        password:"",
-        profilePic:"",
-        isEditing:false,
-        firstCreated:Date(),
-        LastEdited:Date()})
+    const [formData,setFormData] = useState<User>(
+        {
+            id:Date.now(),
+            username:"",
+            email:"",
+            password:"",
+            profilePic:"",
+            isEditing:false,
+            firstCreated:Date(),
+            LastEdited:Date()
+        }
+    )
+    function handleChangeInputFields(){
+
+    }
   return (
     <React.Fragment>
         <div className="flex flex-col">
             <img src={formData.profilePic}/>
         </div>
         <div className="flex flex-col">
-            <label htmlFor="username">ddddddd</label>
-            <input type="text" id="username" value={formData.username}/>
+            <label htmlFor="username">enter username</label>
+            <input type="text" id="username" onChange={handleChangeInputFields} value={formData.username}/>
         </div>
         <div className="flex flex-col">
-            <label htmlFor="email">ddddddd</label>
-            <input type="email" id="email" value={formData.email}/>
+            <label htmlFor="email">enter email</label>
+            <input type="email" id="email" onChange={handleChangeInputFields} value={formData.email}/>
         </div>
         <div className="flex flex-col">
-            <label htmlFor="password">ddddddd</label>
-            <input type="email" id="password" value={formData.password}/>
+            <label htmlFor="password">enter password</label>
+            <input type="email" id="password" onChange={handleChangeInputFields} value={formData.password}/>
         </div>
     </React.Fragment>
   )
