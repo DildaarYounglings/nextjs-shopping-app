b8414cca554350e18e820151198dc47ff0dcba69
primedildaar2 <primedildaar2@gmail.com>
2024-03-11 09:15:25 +0200

    first commit for the day

---
+++ app/auth/page.tsx
@@ -0,0 +1,14 @@
+
+import ProductsPage from '@/components/ProductsPage';
+import React from 'react';
+
+export default function Page(){
+
+  return(
+    <React.Fragment>
+      <main className="flex absolute left-1/2 top-1/2 w-full" style={{transform:"translateX(-50%) translateY(-50%)",padding:"1rem",margin:"0px auto"}}>
+        ddddddddddddddd
+      </main>
+    </React.Fragment>
+  )
+}
