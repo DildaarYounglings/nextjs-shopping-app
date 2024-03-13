43ec15331d90dc1998423cb97f1ba285f3a91b9b
primedildaar2 <primedildaar2@gmail.com>
2024-03-13 14:45:11 +0200

    testing application and features

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,127 +5,140 @@ import React, { useEffect, useState } from "react";
 
 function Nav() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
-  const {windowDimensions} = useWindowDimensions()
-  if (windowDimensions.width >= 600) {
+  const { windowDimensions } = useWindowDimensions();
+  if (windowDimensions.width < 600) {
     return (
       <div
-        className="w-full flex flex-row gap-3 fixed top-0 left-0"
+        className="w-full flex flex-col gap-3 fixed top-0 left-0 cursor-pointer"
         style={{ backgroundColor: "darkcyan", padding: "1rem" }}
       >
-        <button style={{ width: "1fr", textAlign: "center" }}>
-          <span style={{ color: "lightgrey" }}>{"< "}</span>
-          {"Outlaw Stickers"}
-          <span style={{ color: "lightgrey" }}>{" />"}</span>
-        </button>
-        <nav
-          className="flex flex-row"
-          style={{ margin: "0px 1rem 0rem auto", gap: "10%" }}
-        >
-          <div className="flex flex-row gap-3" style={{ padding: "0.5rem" }}>
-            <Link
-              style={{
-                backgroundColor: "lightcyan",
-                width: "1fr",
-                height: "fit-content",
-                padding: "0.5rem",
-                textAlign: "center",
+        {isOpen ? (
+          <>
+            <button
+              onClick={() => {
+                setIsOpen(!isOpen);
               }}
-              href="/"
+              style={{ width: "1fr", textAlign: "center" }}
             >
-              Home
-            </Link>
-            <Link
-              style={{
-                backgroundColor: "lightcyan",
-                width: "1fr",
-                height: "fit-content",
-                padding: "0.5rem",
-                textAlign: "center",
+              <span style={{ color: "lightgrey" }}>{"< "}</span>
+              {"Outlaw Stickers"}
+              <span style={{ color: "lightgrey" }}>{" />"}</span>
+            </button>
+            <nav className="flex flex-col gap-3">
+              <Link
+                style={{
+                  backgroundColor: "lightcyan",
+                  width: "1fr",
+                  height: "fit-content",
+                  padding: "0.7rem",
+                  textAlign: "center",
+                }}
+                href="/"
+              >
+                Home
+              </Link>
+              <Link
+                style={{
+                  backgroundColor: "lightcyan",
+                  width: "1fr",
+                  height: "fit-content",
+                  padding: "0.7rem",
+                  textAlign: "center",
+                }}
+                href="/shop"
+              >
+                Products
+              </Link>
+              <div
+                style={{
+                  color: "white",
+                  borderRadius: "25%",
+                  width: "1fr",
+                  height: "fit-content",
+                  display: "flex",
+                  gap: "1rem",
+                  margin: "0px auto",
+                }}
+              >
+                <Link
+                  href="/signIn"
+                  style={{
+                    backgroundColor: "black",
+                    width: "1fr",
+                    padding: "0.5rem",
+                  }}
+                >
+                  <input type="button" value="Sign In" />
+                </Link>
+                <Link
+                  href="/signOut"
+                  style={{
+                    backgroundColor: "black",
+                    width: "1fr",
+                    padding: "0.5rem",
+                  }}
+                >
+                  <input type="button" value="Sign Out" />
+                </Link>
+              </div>
+            </nav>
+          </>
+        ) : (
+          <>
+            <p
+              onClick={() => {
+                setIsOpen(!isOpen);
               }}
-              href="/shop"
             >
-              Products
-            </Link>
-          </div>
-          <div
-            style={{
-              color: "white",
-              borderRadius: "25%",
-              width: "1fr",
-              height: "fit-content",
-              display: "flex",
-              gap: "1rem",
-              margin: "0px auto",
-              padding: "0.5rem",
-            }}
-          >
-            <Link
-              href="/signIn"
-              style={{
-                backgroundColor: "black",
-                width: "1fr",
-                padding: "0.5rem",
-              }}
-            >
-              <input type="button" value="Sign In" />
-            </Link>
-            <Link
-              href="/signOut"
-              style={{
-                backgroundColor: "black",
-                width: "1fr",
-                padding: "0.5rem",
-              }}
-            >
-              <input type="button" value="Sign Out" />
-            </Link>
-          </div>
-        </nav>
+              <span style={{ color: "lightgrey" }}>{"< "}</span>
+              {"Outlaw Stickers"}
+              <span style={{ color: "lightgrey" }}>{" />"}</span>
+            </p>
+          </>
+        )}
       </div>
     );
   }
   return (
     <div
-  className="w-full flex flex-col gap-3 fixed top-0 left-0 cursor-pointer"
-  style={{ backgroundColor: "darkcyan", padding: "1rem" }}
->
-  {isOpen ? (
-    <>
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        style={{ width: "1fr", textAlign: "center" }}
-      >
+      className="w-full flex flex-row gap-3 fixed top-0 left-0"
+      style={{ backgroundColor: "darkcyan", padding: "1rem" }}
+    >
+      <button style={{ width: "1fr", textAlign: "center" }}>
         <span style={{ color: "lightgrey" }}>{"< "}</span>
         {"Outlaw Stickers"}
         <span style={{ color: "lightgrey" }}>{" />"}</span>
       </button>
-      <nav className="flex flex-col gap-3">
-        <Link
-          style={{
-            backgroundColor: "lightcyan",
-            width: "1fr",
-            height: "fit-content",
-            padding: "0.7rem",
-            textAlign: "center",
-          }}
-          href="/"
-        >
-          Home
-        </Link>
-        <Link
-          style={{
-            backgroundColor: "lightcyan",
-            width: "1fr",
-            height: "fit-content",
-            padding: "0.7rem",
-            textAlign: "center",
-          }}
-          href="/shop"
-        >
-          Products
-        </Link>
+      <nav
+        className="flex flex-row"
+        style={{ margin: "0px 1rem 0rem auto", gap: "10%" }}
+      >
+        <div className="flex flex-row gap-3" style={{ padding: "0.5rem" }}>
+          <Link
+            style={{
+              backgroundColor: "lightcyan",
+              width: "1fr",
+              height: "fit-content",
+              padding: "0.5rem",
+              textAlign: "center",
+            }}
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            style={{
+              backgroundColor: "lightcyan",
+              width: "1fr",
+              height: "fit-content",
+              padding: "0.5rem",
+              textAlign: "center",
+            }}
+            href="/shop"
+          >
+            Products
+          </Link>
+        </div>
         <div
           style={{
             color: "white",
@@ -135,6 +148,7 @@ function Nav() {
             display: "flex",
             gap: "1rem",
             margin: "0px auto",
+            padding: "0.5rem",
           }}
         >
           <Link
@@ -159,22 +173,8 @@ function Nav() {
           </Link>
         </div>
       </nav>
-    </>
-  ) : (
-    <>
-      <p
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        <span style={{ color: "lightgrey" }}>{"< "}</span>
-        {"Outlaw Stickers"}
-        <span style={{ color: "lightgrey" }}>{" />"}</span>
-      </p>
-    </>
-  )}
-</div>
-
+    </div>
   );
 }
+
 export default Nav;
--- components/UserProfilePage.tsx
+++ components/UserProfilePage.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 export const UserProfilePage = function(){
   return (
     <div>UserProfilePage</div>
-  )
+    )
 }
