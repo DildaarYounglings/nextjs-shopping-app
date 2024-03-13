376daefbe2097b1cf29c798968672c2b008880b9
primedildaar2 <primedildaar2@gmail.com>
2024-03-13 12:05:15 +0200

    testing env but not working

--- firebase/authentication.ts
+++ firebase/authentication.ts
@@ -6,11 +6,23 @@ import { exportTraceState } from "next/dist/trace";
 import {User, deleteUser, getAuth} from "firebase/auth";
 import { use } from "react";
 import { mustGetValue } from "@/utils/must-get-value";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvK78-xcR9dtiZ-jBfI5Mp3vTF9uqfuJs",
+    authDomain: "shopping-app-nextjs.firebaseapp.com",
+    projectId: "shopping-app-nextjs",
+    storageBucket: "shopping-app-nextjs.appspot.com",
+    messagingSenderId: "68973032878",
+    appId: "1:68973032878:web:446a37ee3ab6f3a57dc43f",
+    measurementId: "G-WPT1MW0PQJ"
+  };
+*/
 const firebaseConfig = {
   apiKey: "AIzaSyBvK78-xcR9dtiZ-jBfI5Mp3vTF9uqfuJs",
   authDomain: "shopping-app-nextjs.firebaseapp.com",
@@ -24,4 +36,5 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig,"my app")
 export const app = firebaseApp as firebase.app.App;
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const user = mustGetValue<typeof auth.currentUser>(auth.currentUser);
