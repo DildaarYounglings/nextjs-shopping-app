f408c2f4ad50d2bc071ba80c8cbf798367432376
primedildaar2 <primedildaar2@gmail.com>
2024-03-14 11:01:23 +0200

    testing profile page and added inport from a hook

--- components/UserProfilePage.tsx
+++ components/UserProfilePage.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { allUserProfileState} from '@/hooks/useStateGlobal'
+import React, { useState } from 'react'
 
 export const UserProfilePage = function(){
+  function handleChange(){
+  }
   return (
-    <div>UserProfilePage</div>
-    )
+    <section>
+      <div>
+        <img src={allUserProfileState.state.imgSrc} alt="UserProfilePic" />
+      </div>
+      <div>
+        {allUserProfileState.state.isEditingUsername && <label>{allUserProfileState.state.username} ✏️</label>}
+        {allUserProfileState.state.isEditingPassword === false ?<label onClick={() => {allUserProfileState.setState((a) => ({...a,isEditingPassword:!allUserProfileState.state.isEditingPassword}))}}>{allUserProfileState.state.password} ✏️</label> : <span className='p-4 flex gap-4'><input type="text" value={allUserProfileState.state.email}/><button onClick={() => {allUserProfileState.setState((a) => ({...a,isEditingEmail:!allUserProfileState.state.isEditingPassword}))}}>cancel</button><button>💾</button></span>}
+        {allUserProfileState.state.isEditingEmail === false ?<label onClick={() => {allUserProfileState.setState((a) => ({...a,isEditingEmail:!allUserProfileState.state.isEditingEmail}))}}>{allUserProfileState.state.email} ✏️</label> : <span className='p-4 flex gap-4'><input type="text" value={allUserProfileState.state.email}/><button onClick={() => {allUserProfileState.setState((a) => ({...a,isEditingEmail:!allUserProfileState.state.isEditingEmail}))}}>cancel</button><button>💾</button></span>}
+      </div>
+    </section>
+  )
 }
