cd7b01e912c81908245fed49b70348c893e7d6f5
DildaarYounglings <dildaar.abrahams@younglings.africa>
2024-05-22 09:13:42 +0200

    created userProfileState

--- globalZustandState/global-state.ts
+++ globalZustandState/global-state.ts
@@ -1,3 +1,4 @@
+import { UserProfile } from "firebase/auth";
 import { create } from "zustand";
 export type CheckoutProduct = {
     stickerName: string,
@@ -8,9 +9,17 @@ export type Checkout_Cart_State = {
     checkoutCart: CheckoutProduct[],
     setCheckoutCart: (setterFunction: (state: Checkout_Cart_State) => Checkout_Cart_State|any) => void,
 }
-
-const checkoutCart: CheckoutProduct[] = [];
 export const useCheckoutCart = create<Checkout_Cart_State>((set) => ({
     checkoutCart,
     setCheckoutCart: (setterFunction) => set(setterFunction)
+}));
+
+export type UserProfileState = {
+    userProfile:any,
+    setUserProfile: (setterFunction: (state:UserProfileState) => Checkout_Cart_State|any) => void,
+}
+const checkoutCart: CheckoutProduct[] = [];
+export const useUserProfile = create<UserProfileState>((set) => ({
+    userProfile:{},
+    setUserProfile: (setterFunction) => set(setterFunction)
 }));
